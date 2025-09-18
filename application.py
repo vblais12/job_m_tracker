a56982c1d21a11d055b6507bf61d60bc59f7797a
vblais12 <vblais122@gmail.com>
2025-09-17 19:19:29 -0700

    Remove AWS deployment files, switch to Railway

--- application.py
+++
@@ -1,9 +0,0 @@
-# Elastic Beanstalk entry point
-from backend.main import app
-
-# WSGI application variable that Elastic Beanstalk expects
-application = app
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(application, host="0.0.0.0", port=8000)
